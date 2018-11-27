eb86cb75386566229c76f2cb88f7d94065e9f957
wl <linj@linjingdeMacBook-Air.local>
2018-11-27 20:15:20 +0800

    create project

---
+++ dabaoji/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  dabaoji
+//
+//  Created by 林静 on 2018/11/27.
+//  Copyright © 2018年 林静. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ dabaojiTests/dabaojiTests.m
@@ -0,0 +1,39 @@
+//
+//  dabaojiTests.m
+//  dabaojiTests
+//
+//  Created by 林静 on 2018/11/27.
+//  Copyright © 2018年 林静. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface dabaojiTests : XCTestCase
+
+@end
+
+@implementation dabaojiTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
